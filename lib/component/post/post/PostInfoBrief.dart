646a7be5f2e2e8a6a07bc759e22bf4b36de9e2ac
ww <193960675@qq.com>
2023-06-25 17:04:00 +0800

    部分发表帖子界面

--- lib/component/post/post/PostInfoBrief.dart
+++ lib/component/post/post/PostInfoBrief.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-/// 帖子界面
+/// 帖子
 /// @author ww
 /// @date 2023-06-24
 
@@ -14,30 +13,93 @@ class PostInfoBrief extends StatefulWidget {
 class PostInfoBriefState extends State<PostInfoBrief> {
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children:[
-        Row(
-          children:[
-          Expanded(child: Text('头像')),
-          Column(
+    return MaterialApp(
+      home: Directionality(
+        textDirection: TextDirection.ltr,
+        child: Scaffold(
+          body: ListView(
             children: [
-            Expanded(child: Text('名字')),
-            Expanded(child: Text('关注')),
-          ],
+              titleRow, //标题
+              Image.asset('assets/image/login.png', fit: BoxFit.cover), //图片
+              titleSection2, //内容
+              titleSection, //地点
+            ],
+          ),
         ),
-      ]),
-      Expanded(child: Text('标题')),
-      Expanded(child: Text('图片')),
-      Expanded(child: Text('描述')),
-      Expanded(child: Text('地点')),
-      Row(
-        children: [
-          Expanded(child: Text('标签')),
-          Expanded(child: Text('点赞数')),
-          Expanded(child: Text('点赞')),
-        ],
-      )
-      ]
+      ),
     );
   }
 }
+
+//头像，名字，关注
+Widget personalSection = Container(
+  padding: const EdgeInsets.all(32),
+  height: 120,
+  child: Row(
+
+    children: [
+      Image.asset('assets/image/login.png', fit: BoxFit.cover),
+      Column(
+        children: [
+          const Text('名字', style: TextStyle(fontSize: 20,),),
+          Expanded(
+            child: Row(
+              children: [
+                Icon(Icons.star, color: Colors.red[500],),
+                const Text('关注'),
+              ],
+            ),
+          ),
+        ],
+      ),
+    ],
+  ),
+);
+
+//单行输入框
+Widget titleSection = const TextField(
+  decoration: InputDecoration(
+    border: OutlineInputBorder(),
+    hintText: 'Enter text here',
+  ),
+  maxLines: 1,
+);
+
+//五行输入框
+Widget titleSection2 = const TextField(
+  decoration: InputDecoration(
+    border: OutlineInputBorder(),
+    hintText: 'Enter text here',
+  ),
+  maxLines: 5,
+);
+
+//tag,点赞，点赞数
+Widget domain = const Row(
+  textDirection: TextDirection.ltr,
+  children: [
+    Expanded(child: Text('标签')),
+    Expanded(child: Text('点赞数')),
+    Expanded(child: Text('点赞')),
+  ],
+);
+
+
+Widget titleRow = const Row(
+  children: [
+    Padding(
+      padding: EdgeInsets.symmetric(horizontal: 8.0),
+      child: Text(
+        '标题:',
+        style: TextStyle(fontSize: 16.0),
+      ),
+    ),
+    Expanded(
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: '输入标题',
+        ),
+      ),
+    ),
+  ],
+);
