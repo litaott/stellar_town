4e7cec86bd77169771d09f52732d78ee54f78803
ww <193960675@qq.com>
2023-06-25 17:34:26 +0800

    color

---
+++ lib/theme/ColorTheme.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+/// 颜色主题
+/// @author tt
+/// @date 2023-06-24
+
+class ColorTheme {
+  ColorTheme._();
+
+  ///蓝色
+  static const Color blue = Color(0xFF2196F3);
+
+  ///浅蓝色
+  static const Color lightBlue = Color(0xFF21CCF3);
+
+  ///深蓝色
+  static const Color deepBlue = Color(0xFF2144F3);
+
+  ///青色
+  static const Color cyan = Color(0xFF21FFF3);
+}
