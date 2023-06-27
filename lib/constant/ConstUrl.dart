2025ee4d09796d71b509fd4c37e89848bd36faaa
李涛 <3600285899@qq.com>
2023-06-27 11:27:24 +0800

    注册测试

--- lib/constant/ConstUrl.dart
+++ lib/constant/ConstUrl.dart
@@ -9,14 +9,14 @@ class ConstUrl {
       'https://up.enterdesk.com/photo/2011-6-4/enterdesk.com-70CD5C7D85E0DD1D1E548CA0CF22FDF1.jpg';
 
   /// 服务器
-  static const String baseUrl = 'http://http://49ilim5biema.xiaomiqiu.com';
+  static const String baseUrl = 'http://49ilim5biema.xiaomiqiu.com';
 
   /// 用户相关
   static const String userBaseUrl = '/api/v1/user';
-  static const String registerUrl = '$userBaseUrl/getUserInfo';
-  static const String loginUrl = '$userBaseUrl/getUserInfo';
+  static const String registerUrl = '$userBaseUrl/register';
+  static const String loginUrl = '$userBaseUrl/login';
   static const String getUserInfoUrl = '$userBaseUrl/getUserInfo';
-  static const String logoutUrl = '$userBaseUrl/getUserInfo';
-  static const String updateUserInfoUrl = '$userBaseUrl/getUserInfo';
-  static const String getUserInfoByIdUrl = '$userBaseUrl/getUserInfo';
+  static const String logoutUrl = '$userBaseUrl/logout';
+  static const String updateUserInfoUrl = '$userBaseUrl/updateUserInfo';
+  static const String getUserInfoByIdUrl = '$userBaseUrl/getUserInfoById';
 }
--- lib/util/HttpUtil.dart
+++ lib/util/HttpUtil.dart
@@ -13,34 +13,74 @@ class HttpUtil {
     receiveTimeout: 5000,
   );
 
+  static late String token;
+
   // Create a new instance of Dio with custom options
-  static Dio createInstance() {
+  static Dio noTokenInstance() {
     final dio = Dio(defaultOptions);
     dio.options.contentType = Headers.jsonContentType;
     return dio;
   }
 
-  // Perform an HTTP GET request
-  static Future<Response> get(String path) async {
-    final dio = createInstance();
-    return dio.get(path);
+  static Dio jsonInstance() {
+    final dio = Dio(defaultOptions);
+    dio.options.contentType = Headers.jsonContentType;
+    dio.options.headers['Authorization'] = token;
+    return dio;
   }
 
-  // Perform an HTTP POST request
-  static Future<Response> post(String path, dynamic data) async {
-    final dio = createInstance();
+  static Dio formDataInstance() {
+    final dio = Dio(defaultOptions);
+    dio.options.contentType = 'multipart/form-data';
+    dio.options.headers['Authorization'] = token;
+    return dio;
+  }
+
+  /// 无token请求
+  static Future<Response> post(String path, Map data) async {
+    final dio = noTokenInstance();
     return dio.post(path, data: data);
   }
 
-  // Perform an HTTP PUT request
-  static Future<Response> put(String path, dynamic data) async {
-    final dio = createInstance();
+  /// Json类型请求
+  static Future<Response> getJson(String path) async {
+    final dio = jsonInstance();
+    return dio.get(path);
+  }
+
+  static Future<Response> postJson(String path, Map data) async {
+    final dio = jsonInstance();
+    return dio.post(path, data: data);
+  }
+
+  static Future<Response> putJson(String path, dynamic data) async {
+    final dio = jsonInstance();
     return dio.put(path, data: data);
   }
 
-  // Perform an HTTP DELETE request
-  static Future<Response> delete(String path) async {
-    final dio = createInstance();
+  static Future<Response> deleteJson(String path) async {
+    final dio = jsonInstance();
+    return dio.delete(path);
+  }
+
+  /// FormData类型请求
+  static Future<Response> getForm(String path) async {
+    final dio = formDataInstance();
+    return dio.get(path);
+  }
+
+  static Future<Response> postForm(String path, Map data) async {
+    final dio = formDataInstance();
+    return dio.post(path, data: data);
+  }
+
+  static Future<Response> putForm(String path, dynamic data) async {
+    final dio = formDataInstance();
+    return dio.put(path, data: data);
+  }
+
+  static Future<Response> deleteForm(String path) async {
+    final dio = formDataInstance();
     return dio.delete(path);
   }
 }
