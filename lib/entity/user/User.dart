94d16f830f88b7134b38444d427fb247a2c6c3bb
李涛 <3600285899@qq.com>
2023-06-28 13:56:56 +0800

    新增currentUser，优化结构

--- lib/entity/user/User.dart
+++ lib/entity/user/User.dart
@@ -9,17 +9,17 @@ part 'User.mapper.dart';
 
 @MappableClass()
 class User with UserMappable {
-  String id; //账号（主键）
+  int id; //账号（主键）
   String username; //用户名
   String password; //密码
   String phoneNumber; //手机号
   String avatar; //头像
   String address; //地址
-  String gender; //性别
-  String age; //年龄
+  int gender; //性别
+  int age; //年龄
   String signature; //个性签名
   String followNumber; //关注数
-  String fansNumber; //粉丝数
+  String fansNumber;
 
   User({
     required this.id,
@@ -28,8 +28,8 @@ class User with UserMappable {
     this.phoneNumber = '未填写',
     this.avatar = ConstUrl.defaultAvatar,
     this.address = '未填写',
-    this.gender = '未填写',
-    this.age = '未填写',
+    this.gender = 2,
+    this.age = 0,
     this.signature = '快来书写你的个性签名吧！',
     this.followNumber = '0',
     this.fansNumber = '0',
@@ -39,18 +39,6 @@ class User with UserMappable {
   static const fromMap = UserMapper.fromMap;
   static const fromJson = UserMapper.fromJson;
 
-  static User defaultUser = User(id: '0', username: '未知用户', password: '0');
-
-  // /// 获取当前用户信息*async
-  // /// @param null
-  // /// @return User
-  // static Future<User?> getCurrentUser() async {
-  //   final prefs = await SharedPreferences.getInstance();
-  //   final userInfo = prefs.getString('userInfo');
-  //   if (userInfo != null) {
-  //     return User.fromJson(userInfo);
-  //   } else {
-  //     return null;
-  //   }
-  // }
+  static User defaultUser = User(id: 0, username: '未知用户', password: '');
+  static late User currentUser;
 }
